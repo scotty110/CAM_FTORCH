af5ed54cc22dc94bbaee8c2f768c66e483c565b1
scotty <scotty@none.com>
2024-08-18 09:44:28 -0400

    status open

--- cesm/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#docker run -it cesm /bin/bash
-#docker run -it -v $HOME/Documents/code/cesm_inputs:/root/cesm cesm /bin/bash
-docker run -it --cpus="8" -v $HOME/Documents/thor_shit/cesm_inputs:/root/cesm cesm /bin/bash
-
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -it -v $HOME/Documents/cesm_inputs:/root/cesm cam_cuda /bin/bash
+
