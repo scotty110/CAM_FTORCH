17bc025eb28d1a28cfe9096b671250f252f6af82
dory <scott@Dory.lan>
2024-09-10 12:41:59 -0400

    Moved Makefile to Copy

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-#docker run -it -v $HOME/Documents/cesm_inputs:/root/cesm cam_cuda /bin/bash
-#    -v $HOME/Documents/CAM:/opt/cesm/components/cam \
-#   cam_cuda /bin/bash
 docker run -it \
-    --runtime=nvidia \
-    -v $HOME/Documents/CAM:/opt/cesm/components/cam \
-    -v $HOME/Documents/cesm_inputs:/root/cesm \
-    -v $HOME/Documents/cam_weights:/weights \
+    -v $HOME/Documents/code/cesm_inputs:/root/cesm \
     cesm_ftorch /bin/bash
 
