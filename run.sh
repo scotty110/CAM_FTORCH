18e344e151ed35f7a99acef0ee52b94863a087e1
Scott <none@gmail.com>
2024-12-23 12:03:36 -0500

    Working no gpu

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker run -it \
-    -v $HOME/Documents/code/cesm_inputs:/root/cesm \
-    cesm_ftorch /bin/bash
+#docker run -it \
+#    -v $HOME/Documents/cesm_inputs:/root/cesm \
+#    cesm_ftorch /bin/bash
 
+
+docker run -it \
+    -v $HOME/Documents/cesm_inputs:/root/cesm \
+    -v $HOME/Documents/CAM:/opt/cesm/components/cam \
+    -v $HOME/Documents/weights:/weights \
+   cesm_ftorch /bin/bash
